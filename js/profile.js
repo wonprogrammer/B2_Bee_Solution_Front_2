aab30c6665d8346b313945738e835127160ca1e8
leehyewon <tnddl2345@gmail.com>
2022-12-09 18:19:58 +0900

    modify:코드정리

--- js/profile.js
+++ js/profile.js
@@ -67,46 +67,6 @@ async function userProfileUpload(){
     )
 }
 
-
-// 로그인한 user 글만 가져오기
-// async function load_user_articles(){
-//     category_id = localStorage.getItem('category_id')
-//     const response = await fetch(`${main_url}/article/${category_id}/`,{
-//         headers : {
-//             "Authorization": "Bearer " + localStorage.getItem("access"),
-//             "content-type": "application/json"
-//         },
-//         method : 'GET',
-//     })
-//     response_json = await response.json()
-//     console.log(response_json)
-
-//     var my_articles = new Array
-//     response_json.forEach(element => {
-//         if (element.user == userId){
-//             my_articles.push(element)
-//         }
-//     })
-
-//     let articles_box = document.getElementById('articles')
-//     let output = ''
-//     my_articles.forEach(element=>{
-        
-//         output += `
-//         <div class="card text-center" style="width: 30rem; margin-top: 10px;">
-//             <div class="card-body">
-//                 <h5 class="card-title">${element.category} / ${element.mbti}</h5>
-//                 <p class="card-text">${element.content}</p>
-//                 <a href="javascript:save_article_id(${element.id});" class="btn btn-outline-secondary">게시글 보기</a>
-//             </div>
-//         </div>
-//         `    
-//     })
-//     articles_box.innerHTML = output
-// }
-
-
-
 async function get_articles(page_param){
     if (page_param == ''){
     const response = await fetch(`${main_url}/article/${category_id}/`,{
@@ -234,3 +194,6 @@ function save_category_id(category_id){
     localStorage.setItem('category_id',category_id)
     window.location.reload()
 }
+
+
+// 비밀번호 변경
